9aaee3db78867a8a85f171a7dd2da9268f29685a
Syed Ahamed <100705207+ilazzy@users.noreply.github.com>
2022-05-11 15:38:39 +0530

    Add files via upload

---
+++ Num-To-Alpha.py
@@ -0,0 +1,29 @@
+import tkinter as tk
+import inflect
+
+frame = tk.Tk()
+frame.title("Title.exe")
+frame.geometry('400x200')
+frame.configure(bg='gold')
+frame1 = tk.Label(frame, text="NUMBERS ONLY !!!",background="gold")
+frame1.pack()
+
+def printInput():
+        inp = inputtxt.get(1.0, "end-1c")
+        n2w = inflect.engine()
+        xx = n2w.number_to_words(inp)
+        lbl.config(text = "Provided Input: "+xx)
+        inputtxt.delete("1.0","end")
+
+# TextBox Creation
+inputtxt = tk.Text(frame,height = 2,width = 25, foreground="black")
+inputtxt.pack()
+
+# Button Creation
+printButton = tk.Button(frame,text = "run",command = printInput)
+printButton.pack()
+
+# Label Creation  
+lbl = tk.Label(frame, text = "",background="gold")
+lbl.pack()
+frame.mainloop()
